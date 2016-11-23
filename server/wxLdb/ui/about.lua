987bb82aa217152e0bcbe053e479b1ffc0b89559
Youen Toupin <youen.toupin@gmail.com>
2016-11-23 13:39:00 +0100

    - updated about.lua - improved documentation

--- server/wxLdb/ui/about.lua
+++ server/wxLdb/ui/about.lua
@@ -7,5 +7,5 @@ _G.print = print -- override wx print function with original one
 module( "ui.about" )
 
 function popup()
-	wx.wxMessageBox( "Graphical Remote Lua Debugger @VERSION@\nCopyright (C) 2010-2012 Youen Toupin.  All rights reserved.\nSee file GRLD-license.txt for permission notice.\nSee file contact.txt if you have any question,\nor go to http://cushy-code.com/grld/", "About GRLD" )
+	wx.wxMessageBox( "Graphical Remote Lua Debugger\nCopyright (C) 2010-2016 Youen Toupin.  All rights reserved.\nSee file LICENSE for permission notice.\nSee github repository https://github.com/neuoy/GRLD if you have any question", "About GRLD" )
 end
